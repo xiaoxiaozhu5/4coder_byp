e2c825f4952b7eae375f4f7ec866f11f051377cc
puzz-h <xiaoxiaozhu5@126.com>
2024-07-11 12:24:07 +0800

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp ./custom_4coder.* ../custom_4coder/
-cp ./custom_4coder.* ../4coder_byp/custom_4coder/
+mkdir -p ../custom_4coder && cp -r ./custom_4coder.* $_
+mkdir -p ../4coder_byp/custom_4coder && cp -r ./custom_4coder.* $_
